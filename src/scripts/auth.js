24e029a555ee0b091b19d99da420c3aac9ac0f89
Antonio Matera <antonio.matera@tangible.is>
2023-10-16 11:13:15 +0200

    Adds netlify identity controls

---
+++ src/scripts/auth.js
@@ -0,0 +1,21 @@
+var user = netlifyIdentity.currentUser();
+var siteDiv = document.querySelector('#auth');
+var NetlifyLoginCtrl = document.querySelector('#identity-menu');
+
+netlifyIdentity.on('login', function(user) {
+  showSite();
+});
+
+netlifyIdentity.on('logout', function(user) {
+  hideSite();
+});
+
+function showSite() {
+  siteDiv.style.display = "block";
+  // NetlifyLoginCtrl.style.display = "none";
+}
+
+function hideSite() {
+  siteDiv.style.display = "none";
+  // NetlifyLoginCtrl.style.display = "block";
+}
